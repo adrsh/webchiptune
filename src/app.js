572503a5714703ba930c62d05688eee87650aad4
Adrian Shosholli <as227cw@student.lnu.se>
2022-09-18 05:21:48 +0200

    Refactor stuff and make it possible to play different notes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,10 @@ import * as Chiptune from './index.js'
 const startButton = document.querySelector('#start-button')
 startButton.addEventListener('click', event => Chiptune.start())
 
-const square = new Chiptune.Synth('square')
-const sawtooth = new Chiptune.Synth('sawtooth')
+const square = new Chiptune.Instrument('square')
 
 const playButton = document.querySelector('#play-button')
-playButton.addEventListener('click', event => square.play())
+playButton.addEventListener('click', event => square.play(63))
 
 const stopButton = document.querySelector('#stop-button')
 stopButton.addEventListener('click', event => square.stop())
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export function start () {
 }
 
 /**
- * Returns the current time.
+ * Returns the current time including a short look ahead time.
  *
  * @returns {number} Current time
  */
@@ -19,32 +19,34 @@ export function now () {
 /**
  *
  */
-export class Synth {
+export class Instrument {
   oscillator
-  gain
+  gainNode
   /**
-   * Constructor for Synth.
+   * Constructor for Instrument.
    *
-   * @param {String} type Synth type.
+   * @param {String} type Oscillator type.
    */
   constructor (type) {
     this.oscillator = new OscillatorNode(context, {
       type: type
     })
-    this.gain = new GainNode(context, { gain: 0 })
-    this.oscillator.connect(this.gain).connect(context.destination)
+    this.gainNode = new GainNode(context, { gain: 0 })
+    this.oscillator.connect(this.gainNode).connect(context.destination)
     this.oscillator.start()
   }
 
   /**
    * Plays a note at a specified time.
    *
+   * @param {Number} note Which note to play. EX. 48.
    * @param {Number} time Time to play the note.
    */
-  play (time = now()) {
-    this.gain.gain.cancelScheduledValues(time)
-    this.gain.gain.setValueAtTime(0.1, time)
-    this.gain.gain.linearRampToValueAtTime(0, time + 1)
+  play (note, time = now()) {
+    this.oscillator.frequency.setValueAtTime(noteToFrequency(note), time)
+    this.gainNode.gain.cancelScheduledValues(time)
+    this.gainNode.gain.setValueAtTime(0.1, time)
+    this.gainNode.gain.linearRampToValueAtTime(0, time + 1)
   }
 
   /**
@@ -53,4 +55,15 @@ export class Synth {
   stop () {
     this.oscillator.stop()
   }
+}
+
+/**
+ * Convert MIDI note to frequency.
+ *
+ * @param {Number} note MIDI note to convert to frequency.
+ * @returns {Number} Frequency corresponding to the note.
+ */
+function noteToFrequency (note) {
+  // Could be replaced with a look-up-table
+  return 440 * 2 ** ((note - 69) / 12)
 }
